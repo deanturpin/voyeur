d418794a7f7dbc49d70598b823350c3759fd5604
deanturpin <git@lvbtn.uk>
2016-08-16 22:25:39 +0100

    add some more commands and use rdbuf

--- wifi.cpp
+++ wifi.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 int main() {
 	using namespace std;
 
-	ifstream i("/proc/interrupts");
+	// MIME type
+	cout << "Content-Type: text/plain\n\n" << endl;
 
-	char c = i.get();
+	// Define some interesting files
+	vector<string> files = {
+		"/sys/class/net/wlp1s0/address"
+	};
 
-	while(i.good()) {
-		cout << c;
-		c = i.get();
+	// And read them
+	for (const auto &f : files) {
+
+		cout << ifstream(f).rdbuf();
+	}
+
+	// Define some interesting commands
+	vector<string> command = {
+		"iwlist wlp1s0 scan",
+		"date",
+		"iwgetid",
+		"ip route",
+		"ping -w 1 8.8.8.8",
+		"ip neighbour",
+		"nc -vz 0.0.0.0 1-20000 2>&1 | grep succeeded",
+	};
+
+	// And run them
+	for (const auto &c : command) {
+
+		const string file =  "/tmp/blah.txt";
+		string _c = c + " > " + file;
+		system (_c.c_str());
+
+		cout << ifstream(file).rdbuf();
+		cout << endl;
 	}
 
 	return 0;
