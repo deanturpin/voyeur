8d0fc1befbd8305fed639eaacc1aa07d324f7e46
deanturpin <git@lvbtn.uk>
2016-08-30 19:58:35 +0100

    create issues from comments

--- wifi.cpp
+++ wifi.cpp
@@ -27,68 +27,42 @@ int main() {
 
 	using namespace std;
 
-	// Interrogate self
-	// stringstream mac;
-	// mac << ifstream("/sys/class/net/wlp1s0/address").rdbuf();
-
-	// Vendor lookup
-	// stringstream oui;
-	// oui << ifstream("/usr/share/ieee-data/oui.txt").rdbuf();
-
 	// AP scan
 	stringstream iwlist = utl::command("iwlist wlp1s0 scan");
 
 	string line;
-	map<string, vector<string>> nodes;
+	map<int, vector<string>> nodes;
 
 	while (getline(iwlist, line)) {
 
 		// Keys we're interested in
-		const vector<string> keys = {"Address", "ESSID"};
-
-		static string essid = "default";
-		// cout << "--- " << line << endl;
+		const vector<string> keys = {"Address", "ESSID", "Encryption key"};
 
 		// Search for the keys
 		for (const auto &k : keys) {
 
+			static int cell = 0;
+
 			if (line.find(k) != string::npos) {
-				cout << "+++" << line << endl;
+
+				if (line.find("Cell") != string::npos)
+					++cell;
+
+				nodes[cell].push_back(line);
 				continue;
 			}
-
-			nodes[essid].push_back(line);
 		}
 	}
 
 	// Dump keys
 	cout << "Number of APs " << nodes.size() << endl;
-	/*
 	for (const auto &n : nodes) {
 
-		cout << endl << n.first << endl;
+		cout << n.first << endl;
 
 		for (const auto &k : n.second)
 			cout << "\t" << k << endl;
 	}
-	*/
-
-	// cout << (n.first.find("Address") != string::npos ? "\n" : "") << n.second << endl;
-
-	// Check vendors
-
-	// http://192.168.0.10:1400/status/controllers
-	// http://192.168.0.10:1400/status
-
-	// Rescan (just the AP names)
-	// cout << "------------------------------" << endl;
-	// cout << utl::command("iwlist wlp1s0 scan | grep ESSID") << endl;
-	// cout << "ping: " << utl::command("ping -w 1 8.8.8.8 1> /dev/null && echo true || echo false") << endl;
-	// cout << utl::command("ip route") << endl;
-	// cout << utl::command("ip neighbour") << endl;
-	// cout << utl::command("curl github.com && echo github yes || echo github no") << endl;
-	// cout << utl::command("nc -vz 0.0.0.0 1-20000 2>&1 | grep succeeded") << endl;
-	// cout << utl::command("hcitool scan") << endl;
 
 	return 0;
 }
